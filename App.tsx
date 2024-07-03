31c2c716f419f2ff713f57390ed5165d5085b7b4
Zlaam <mrzalaam@gmail.com>
2024-07-03 23:18:09 +0500

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,50 +1,16 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, useColorScheme } from 'react-native';
+import {} from 'react';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 
-function App(): JSX.Element {
-  const theme = useColorScheme();
-  const [darkMode, setDarkMode] = useState(theme);
-  const modeToggler = () => {
-    setDarkMode(darkMode === 'dark' ? 'light' : 'dark');
-  };
+function App() {
   return (
     <>
-      <View style={darkMode === 'dark' ? styles.blackBg : styles.whiteBg}>
-        <Text style={darkMode === 'dark' ? styles.whiteText : styles.blackText}>
-          Hello world how are you doing?
-        </Text>
-        <Button
-          title={darkMode === 'dark' ? 'Light Mode' : 'Dark Mode'}
-          onPress={modeToggler}
-        />
-      </View>
+      <SafeAreaView>
+        <ScrollView>
+          <Text>FlatCards</Text>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 }
 
 export default App;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  whiteText: {
-    color: '#ffffff',
-  },
-  whiteBg: {
-    backgroundColor: '#ffffff',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  blackBg: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#000000',
-  },
-  blackText: {
-    color: '#000000',
-  },
-});
