f8a547bd919c167e1ac7ed1797e8e6459e585a28
Zlaam <mrzalaam@gmail.com>
2024-07-05 22:29:33 +0500

    update 4 files and create 1 file

--- android/app/src/main/java/com/awesome01/MainApplication.kt
+++ android/app/src/main/java/com/awesome01/MainApplication.kt
@@ -10,6 +10,8 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.mkuczera.RNReactNativeHapticFeedbackPackage;
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -18,7 +20,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(new RNReactNativeHapticFeedbackPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
