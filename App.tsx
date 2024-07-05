79347777c9d9b541cf480fd7176367da0c7b5e8b
Zlaam <mrzalaam@gmail.com>
2024-07-05 17:29:09 +0500

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,54 @@
-import {} from 'react';
-import { View, Text } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
 
-function App() {
+export default function App(): JSX.Element {
+  const [randomBackground, setRandomBackground] = useState('#ffffff');
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    return setRandomBackground(color as string);
+  };
   return (
     <>
-      <View>
-        <Text>Hello World</Text>
+      <StatusBar backgroundColor={randomBackground} />
+      <View style={[styles.container, { backgroundColor: randomBackground }]}>
+        <TouchableOpacity style={styles.bgChangeBtn} onPress={generateColor}>
+          <Text style={styles.bgBtnText}>Change Bg</Text>
+        </TouchableOpacity>
+        <Text selectable>{randomBackground}</Text>
       </View>
     </>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bgChangeBtn: {
+    backgroundColor: '#28282b',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: 140,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    marginVertical: 20,
+  },
+  bgBtnText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
