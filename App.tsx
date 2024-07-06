6c095d48534f17e9cae819f3f9d600f3b0385c2c
Zlaam <mrzalaam@gmail.com>
2024-07-06 14:52:52 +0500

    create 1 file, update 1 file and delete 4 files

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {} from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <>
+      <View>
+        <Text>Open app.tsx and build your first app</Text>
+      </View>
+    </>
+  );
+}
+
+export default App;
