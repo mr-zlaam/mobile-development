79b5b8402f6aa01d5cc8792ff7bc71e9adfb8c9d
Zlaam <mrzalaam@gmail.com>
2024-07-06 14:49:40 +0500

    build(deps): create 1 file and update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,142 @@
-import {} from 'react';
-import { View, Text } from 'react-native';
-
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import Snackbar from 'react-native-snackbar';
+//constants
+import { currencyByRupee } from './constants';
+import CurrencyButton from './components/CurrencyButton';
+//components
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
+  const [targetCurrency, setTargetCurrency] = useState('');
+  const buttonPressed = (targetValue: Currency) => {
+    if (!inputValue)
+      return Snackbar.show({
+        text: 'Enter value to convert currency',
+        backgroundColor: '#28282b',
+        textColor: '#ffffff',
+      });
+    const inputAmount = parseFloat(inputValue);
+    if (!isNaN(inputAmount)) {
+      const convertedValue = inputAmount * Number(targetValue.value);
+      const result = `${targetValue.symbol} ${convertedValue.toFixed(2)} `;
+      setResultValue(result);
+      setTargetCurrency(targetValue.name as string);
+    } else
+      return Snackbar.show({
+        text: 'Not a valid number to convert',
+        backgroundColor: '#28282b',
+        textColor: '#ffffff',
+      });
+  };
   return (
     <>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <>
+        <StatusBar backgroundColor={'#28282b'} />
+        <View style={styles.container}>
+          <View style={styles.topContainer}>
+            <View style={styles.rupeesContainer}>
+              <Text style={styles.rupee}>RS</Text>
+              <TextInput
+                maxLength={14}
+                value={inputValue}
+                clearButtonMode="always"
+                onChangeText={setInputValue}
+                keyboardType="numeric"
+                placeholder="Enter Amount in rupees"
+              />
+            </View>
+            {resultValue && <Text style={styles.resultTxt}>{resultValue}</Text>}
+          </View>
+          <View style={styles.bottomContainer}>
+            <FlatList
+              numColumns={3}
+              data={currencyByRupee}
+              keyExtractor={(item) => item.name?.toString() as string}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() => buttonPressed(item)}
+                  style={[
+                    styles.button,
+                    targetCurrency === item.name && styles.selected,
+                  ]}
+                >
+                  <CurrencyButton {...item} />
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        </View>
+      </>
     </>
   );
 }
 
 export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    elevation: 2,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+  },
+  selected: {
+    backgroundColor: '#ffeaa7',
+  },
+});
