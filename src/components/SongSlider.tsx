7c477d86e6274f361cde8c10bbad0936c00e8ee1
Zlaam <mrzalaam@gmail.com>
2024-07-07 18:15:02 +0500

    feat: create SongInfo.tsx and SongSlider.tsx

---
+++ src/components/SongSlider.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Slider from '@react-native-community/slider';
+import { useProgress } from 'react-native-track-player';
+export default function SongSlider() {
+  const { position, duration } = useProgress();
+
+  return (
+    <View>
+      <Slider
+        value={position}
+        minimumValue={0}
+        maximumValue={duration}
+        thumbTintColor="#28282b"
+        maximumTrackTintColor="#28282b"
+        style={styles.sliderContainer}
+      />
+      <View style={styles.timeContainer}>
+        <Text style={styles.time}>
+          {new Date(position * 1000).toISOString().substring(15, 19)}
+        </Text>
+        <Text style={styles.time}>
+          {new Date((duration - position) * 1000)
+            .toISOString()
+            .substring(15, 19)}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sliderContainer: {
+    width: 350,
+    height: 40,
+    marginTop: 25,
+
+    flexDirection: 'row',
+  },
+  timeContainer: {
+    width: 340,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  time: {
+    color: '#fff',
+  },
+});
